7f367876ebf2d8712038974d41d2f306a64d1cda
David N <nguyen.dddavid@hotmail.com>
2024-01-03 16:44:01 -0500

    Exercices 1 and 2 DONE

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+let repeatString = function(string, number) {
+    repeatString = '';
+    string = string.toString();
+    if (string === '') {
+        return '';
+    }
+    
+    if (number < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < number; i++) {
+            repeatString += string;
+        }
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
