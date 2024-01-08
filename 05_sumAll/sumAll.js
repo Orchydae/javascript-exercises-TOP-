eda4d1eec09b14fe455c910663e643b5d5853fba
David N <nguyen.dddavid@hotmail.com>
2024-01-08 17:42:35 -0500

    Finished exo 3-4-5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    
+    let smallestNumber = Math.min(number1, number2);
+    let largestNumber = Math.max(number1, number2);
+    let totalNumberofTerms = Math.abs(number2 - number1) + 1;
+    let sumOfArithmeticSequence = (totalNumberofTerms/2)*(2*smallestNumber + (totalNumberofTerms - 1));
+    return sumOfArithmeticSequence;
+    
 };
 
 // Do not edit below this line
