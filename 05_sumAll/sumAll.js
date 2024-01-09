bbe2a3a106d5c5686cdd05b46df7af9a88e1fab4
David N <nguyen.dddavid@hotmail.com>
2024-01-08 19:19:40 -0500

    EXO 5 DONE

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
 const sumAll = function(number1, number2) {
     
+    if (number1 < 0 || number2 < 0) {
+        return "ERROR";
+    } else if (typeof number1 != "number" || typeof number2 != "number") {
+        return "ERROR";
+    }
+
     let smallestNumber = Math.min(number1, number2);
     let largestNumber = Math.max(number1, number2);
     let totalNumberofTerms = Math.abs(number2 - number1) + 1;
