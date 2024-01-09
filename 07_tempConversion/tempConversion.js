63ea513fd89582e06a96f77afb1708b3391606c7
David N <nguyen.dddavid@hotmail.com>
2024-01-08 22:43:08 -0500

    EXO 6-7 DONE

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5/9);
+  roundedCelsius = celsius.toFixed(1);
+  return parseFloat(roundedCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius * (9/5)) + 32;
+  roundedFahrenheit = fahrenheit.toFixed(1);
+  return parseFloat(roundedFahrenheit);
 };
 
 // Do not edit below this line
